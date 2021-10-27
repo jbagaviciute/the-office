e848e72842ee0b8ff07f0b659bd276683bd51ac5
jbagaviciute <90718409+jbagaviciute@users.noreply.github.com>
2021-10-27 23:00:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeTab(target, name){
+  var tabs = document.getElementsByClassName("tab");
+  var tab = document.getElementById(name);
+  var buttons = document.getElementsByTagName("button");
+  for(var i = 0; i < tabs.length; i++){
+    tabs[i].style.display = "none";
+    tab.style.display = "block";
+    buttons[i].classList.remove("active");
+  }
+  target.currentTarget.className = "active";
+}
+
+function changeBackground(color){
+  document.body.style.background = color;
+}
